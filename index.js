3951c81bfd265d867a794921ea031b25fac6319c
pradika <handukaagus108@gmail.com>
2023-10-23 01:30:47 +0800

    eror di gambar

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const ejs = require('ejs')
+
+//menampilkan file ejs
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'));
+
 
 
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
+
+app.get('/login', (req, res) => {
+    res.render('pages/login'); // Render tampilan EJS dalam folder 'home'
+  });
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
